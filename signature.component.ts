f662e22a83d7fb3cae2ed05c9dd64db031937b4f
Léon Pelletier <leon.pelletier@apcurium.com>
2018-04-13 12:09:28 -0400

    Some cleanup and fix iPad events

--- signature.component.ts
+++ signature.component.ts
@@ -11,66 +11,60 @@ export class SignatureComponent implements OnInit {
 
   segments: Segment[] = [];
   @Output() savedSignatureAsDataUrl: EventEmitter<string> = new EventEmitter<string>();
-  isFocused: boolean;
 
   get canCancel(): boolean { return this.segments.length === 0; }
 
   private get canvas(): HTMLCanvasElement { return this.canvasElementRef.nativeElement; }
   private get context(): CanvasRenderingContext2D { return this.canvas.getContext("2d"); }
-  private isMouseDown: boolean;
+  private isTouchingDown: boolean;
+  private isTouchingInside: boolean;
   private previousUserSelectStyle: string;
 
-  @HostListener("window:resize", ["$event"]) onWindowResize =
-    ($event: UIEvent) => this.resizeCanvas()
+  @HostListener("window:resize", ["$event"])
+  onWindowResize = ($event: UIEvent) => this.resizeCanvas()
 
-  @HostListener("document:click", ["$event"]) onClickOutsideSignature =
-    ($event: TouchEvent) => this.setFocus(this.canvas.contains($event.target as Node))
+  @HostListener("document:mousedown", ["$event"])
+  @HostListener("document:mousemove", ["$event"])
+  @HostListener("document:touchstart", ["$event"])
+  @HostListener("document:touchmove", ["$event"])
+  touchOutside = ($event: MouseEvent) => this.handleTouchOutside($event, false)
 
-  @HostListener("document:mousedown", ["$event"]) onDocumentMouseDown =
-    ($event: MouseEvent) => this.setFocus(this.canvas.contains($event.target as Node))
-
-  @HostListener("document:mousemove", ["$event"]) onDocumentMouseMove =
-    ($event: MouseEvent) => this.isFocused = this.canvas.contains($event.target as Node)
-
-  @HostListener("document:mouseup", ["$event"]) onDocumentMouseUp =
-    ($event: MouseEvent) => this.drawEnd()
-
-  @HostListener("document:touchstart", ["$event"]) onDocumentTouchStart =
-    ($event: TouchEvent) => this.setFocus(this.canvas.contains($event.target as Node))
-
-  @HostListener("document:touchmove", ["$event"]) onDocumentTouchMove =
-    ($event: TouchEvent) => this.isFocused = this.canvas.contains($event.target as Node)
-
-  @HostListener("document:touchend", ["$event"]) onDocumentTouchEnd =
-    ($event: TouchEvent) => this.drawEnd()
+  @HostListener("document:click", ["$event"])
+  @HostListener("document:mouseup", ["$event"])
+  @HostListener("document:touchend", ["$event"])
+  touchUpOutside = ($event: MouseEvent) => this.handleTouchOutside($event, true)
 
   @HostListener("document:scroll", ["$event"]) onDocumentScroll =
     ($event: MouseEvent) => {
-      if (this.isFocused) {
+      if (this.isTouchingInside) {
         $event.preventDefault();
       }
     }
 
   @HostListener("document:wheel", ["$event"]) onDocumentWheel =
     ($event: MouseWheelEvent) => {
-      if (this.isFocused) {
+      if (this.isTouchingInside) {
         $event.preventDefault();
       }
     }
 
-  @HostListener("touchstart", ["$event"]) ontouchstart =
-    ($event: TouchEvent) => this.drawDown(this.getTouchLocation($event.touches[0]))
+  @HostListener("touchstart", ["$event"])
+  ontouchstart = ($event: TouchEvent) => this.drawDown($event, true)
 
-  @HostListener("touchmove", ["$event"]) ontouchmove =
-    ($event: TouchEvent) => this.drawMove(this.getTouchLocation($event.touches[0]))
+  @HostListener("touchmove", ["$event"])
+  ontouchmove = ($event: TouchEvent) => this.drawMove($event, true)
 
-  @HostListener("touchend", ["$event"]) ontoucheend = ($event: TouchEvent) => this.isMouseDown = false;
+  @HostListener("touchend", ["$event"])
+  ontoucheend = ($event: TouchEvent) => this.drawEnd()
 
-  @HostListener("mousedown", ["$event"]) onmousedown =
-    ($event: MouseEvent) => this.drawDown(this.getTouchLocation($event))
+  @HostListener("mousedown", ["$event"])
+  onmousedown = ($event: MouseEvent) => this.drawDown($event, false)
 
-  @HostListener("mousemove", ["$event"]) onmousemove =
-    ($event: MouseEvent) => this.drawMove(this.getTouchLocation($event))
+  @HostListener("mousemove", ["$event"])
+  onmousemove = ($event: MouseEvent) => this.drawMove($event, false)
+
+  @HostListener("mouseup", ["$event"])
+  onmouseup = ($event: MouseEvent) => this.drawEnd()
 
   constructor(private element: ElementRef) {}
 
@@ -89,32 +83,42 @@ export class SignatureComponent implements OnInit {
     this.refreshCanvas();
   }
 
-  private drawDown(point: Point): void {
+  private drawDown($event: any, isTouch: boolean): void {
+    const touchLocationEvent: TouchLocationEvent = isTouch ? $event.touches[0] : $event;
+    const point: Point = this.getTouchLocation(touchLocationEvent);
     (document.body as HTMLElement).style.userSelect = 'none';
-    this.isMouseDown = true;
+    this.isTouchingDown = true;
     this.segments.push({ points: [point] });
     this.refreshCanvas();
   }
 
-  private drawMove(point: Point): void {
-    if (!this.isMouseDown) { return; }
+  private drawMove($event: any, isTouch: boolean): void {
+    if (!this.isTouchingDown) { return; }
+    const touchLocationEvent: TouchLocationEvent = isTouch ? $event.touches[0] : $event;
+    const point: Point = this.getTouchLocation(touchLocationEvent);
     this.segments[this.segments.length - 1].points.push(point);
     this.refreshCanvas();
   }
 
-  private drawEnd = () => this.isMouseDown = false;
-
-  private setFocus(focused: boolean): void {
-     this.isFocused = focused;
-     if (!focused) {
-       this.isMouseDown = false;
-       (document.body as HTMLElement).style.userSelect = this.previousUserSelectStyle;
-     }
+  private drawEnd(): void {
+    this.isTouchingDown = false;
+    (document.body as HTMLElement).style.userSelect = this.previousUserSelectStyle;
   }
 
-  save = () => this.savedSignatureAsDataUrl.emit(this.canvas.toDataURL());
+  private handleTouchOutside($event: any, up: boolean): boolean {
+    const focused: boolean = this.canvas.contains($event.target as Node);
+     this.isTouchingInside = focused;
+     if (!focused && up) {
+       this.drawEnd();
+     }
+     return !focused;
+  }
 
-  clear = () => {
+  save(): void {
+    this.savedSignatureAsDataUrl.emit(this.canvas.toDataURL());
+  }
+
+  clear(): void {
     this.segments = [];
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
@@ -129,7 +133,7 @@ export class SignatureComponent implements OnInit {
     this.context.stroke();
   }
 
-  private getTouchLocation(touchLocation: TouchLocation): Point {
+  private getTouchLocation(touchLocation: TouchLocationEvent): Point {
     const rect: ClientRect = this.canvas.getBoundingClientRect();
     return {
       x: touchLocation.clientX - rect.left,
@@ -138,7 +142,7 @@ export class SignatureComponent implements OnInit {
   }
 }
 
-export interface TouchLocation {
+export interface TouchLocationEvent {
   clientX: number;
   clientY: number;
 }
